634557e5e2162f73d3ee9ff9b6766fde88b243c9
Rade Micov <80459494+m1cov@users.noreply.github.com>
2023-12-25 09:51:15 +0100

    negativen datum popraven

--- js/script.js
+++ js/script.js
@@ -13,10 +13,10 @@ document.addEventListener("DOMContentLoaded", function () {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    days = days < 10 ? "0" + days : days;
-    hours = hours < 10 ? "0" + hours : hours;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
+    days = days >= 0 ? (days < 10 ? "0" + days : days) : "00";
+    hours = hours >= 0 ? (hours< 10 ? "0" + hours : hours) : "00";
+    minutes = minutes >= 0 ? (minutes < 10 ? "0" + minutes : minutes) : "00";
+    seconds = seconds >= 0 ? (seconds < 10 ? "0" + seconds : seconds) : "00";
 
     document.getElementById("days").innerHTML = days;
     document.getElementById("hours").innerHTML = hours;
